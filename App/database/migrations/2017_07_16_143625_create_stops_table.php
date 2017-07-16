af96b2da0f4c0a3e6603fca3ee273bb3c89628f2
Daebrion Hopwood <daebrion.hopwood.other@gmail.com>
2017-07-16 12:50:19 -0400

    Phase 1 complete

---
+++ App/database/migrations/2017_07_16_143625_create_stops_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('stops', function (
+            Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tour_id')->unsigned();
+            $table->foreign('tour_id')->references('id')->on('tours');
+            $table->datetime('when');
+            $table->string('where');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('stops');
+    }
+}
